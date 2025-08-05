ff6f3b717166153440f79f27f95e8f24aea44b3e
joiicebatista <joiicebatista123@gmail.com>
2025-08-05 17:12:53 -0300

    chore: add better auth tables

--- bewear-ecommerce/src/db/index.ts
+++ bewear-ecommerce/src/db/index.ts
@@ -2,4 +2,7 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+import * as schema from "./schema"; // Adjust the import path as necessary
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema,
+});
